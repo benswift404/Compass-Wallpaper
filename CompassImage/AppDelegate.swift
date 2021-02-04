bf8ed4553cabffba77a221a21fe654e1caf5ec7b
Ben Swift <swiftmac234@gmail.com>
2021-02-04 09:52:38 -0500

    Initial Commit

---
+++ CompassImage/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CompassImage
+//
+//  Created by Ben Swift on 2/4/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CompassImage/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CompassImage
+//
+//  Created by Ben Swift on 2/4/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
